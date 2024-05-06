ff071dc52d5883f105c18360350eb86c3f2f7280
Gustavo Simplício Bernardo <gustavosimpliciob@gmail.com>
2024-05-06 18:06:48 -0300

    Merge branch 'main' of https://github.com/Lohannz/Trabalho-Posner-game
    não faço a mínima ideia doq eu to fazendo

--- combate.h
+++ combate.h
@@ -7,40 +7,74 @@ int rolagem_dado(int faces){
 
 void combate(PERSONAGEM *atacante, INIMIGO*defensor){
     int turno=0;
-    while(defensor->HP > 0 || atacante->HP > 0){
-    
-        printf("\nVoce entrou em batalha com %s\n", defensor->nome);
+    printf("Voce achou um %s!\n", defensor->nome);
+    printf("\nseu HP:%d\nHP do monstro:%d\n", atacante->HP, defensor->HP);
+    while(defensor->HP > 0 && atacante->HP > 0){
+        int escolha = 0;
         int dano = 0;
-    
         int dado1 = rolagem_dado(6);
         int dado2 = rolagem_dado(6);
 
-        printf("\ndado1:%d\ndado2:%d\n", dado1, dado2);
-        if(atacante->SPD > defensor->SPD){
-            dano = (defensor->DEF - atacante->ATK)/2;
+        printf("\n|1-Atacar\n");
+        printf("|2-Tentar fugir!\n");
+        scanf("%i", &escolha);
+        limpar_tela();
+        if(escolha == 1){
+            if(atacante->SPD > defensor->SPD){
+                dano = (defensor->DEF - atacante->ATK)/2;
             if(dano < 1){
                 dano = 1;
             } 
             defensor->HP = defensor->HP - dano;
             printf("\nseu HP:%d\nHP do monstro:%d\n", atacante->HP, defensor->HP);
-        }else if(defensor->SPD > atacante->SPD){
+            sleep(1);
+        }
+        else if(defensor->SPD > atacante->SPD){
             dano = (atacante->DEF - defensor->ATK)/2;
             if(dano < 1){
                 dano = 1;
             }
             atacante->HP = atacante->HP - dano;
             printf("\nseu HP:%d\nHP do monstro:%d\n", atacante->HP, defensor->HP);
-        }else if(atacante->SPD == defensor->SPD){
+            
+        }
+        else if(atacante->SPD == defensor->SPD){
             atacante->SPD = rolagem_dado(6);
-            atacante->SPD = rolagem_dado(6);
+            defensor->SPD = rolagem_dado(6);
         }
         
         if(turno % 2 == 0){
-			int temp = atacante->SPD;
-        	atacante->SPD = defensor->SPD;
-        	defensor->SPD = temp;
-		}
-		
+            int temp = atacante->SPD;
+            atacante->SPD = defensor->SPD;
+            defensor->SPD = temp;
+            sleep(1);
+        }
+        turno++;    
     }
-    turno++;
+    
+    if(escolha == 2){
+        dado1 = rolagem_dado(6);
+        dado2 = rolagem_dado(6);
+        if(dado1 > dado2){
+            printf("\nConseguiu fugir pelo ladinho hehe");
+            break;
+        }
+        else  if(dado2 > dado1){
+            printf("\nNao conseguiu. Se fodeu!");
+            escolha = 1;
+            continue;
+            
+        }
+        else if(dado1 == dado2){
+            printf("\nVoce quase conseguiu!");
+            escolha = 1;
+            continue;
+        }
+    limpar_tela();
+
+    
+    }
+        }
+        
+    pausar();
 }
--- main.c
+++ main.c
@@ -1,12 +1,15 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
+#include<time.h>
 #include "struct.h"
 #include "combate.h"
 #include "logo.h"
 
+int pagina = 1;
 #define TAM_TEX_MAXIMO 400
 
+void imprimir_menu();
 void save();// funções que gravam e carregam informações do jogo
 void load();//
 void menu_principal();//menu principal do jogo
@@ -22,28 +25,68 @@ PERSONAGEM *criarPersonagem();//cria personagem
 PERSONAGEM *novo_personagem;
 
 int main(){
+	int rodando = 1;
 	
 	INIMIGO esqueleto;
 	strcpy(esqueleto.nome,"esqueleto");
 	esqueleto.id = 1;
-	esqueleto.HP = 100;
-	esqueleto.ATK = 20;
-	esqueleto.DEF = 10;
+	esqueleto.HP = 10;
+	esqueleto.ATK = 2;
+	esqueleto.DEF = 1;
 	esqueleto.SPD = 10;
-	esqueleto.posicao = 1;
+	esqueleto.posicao = 0;
 
 	menu_principal();
-	combate(novo_personagem, &esqueleto); 
+	combate(novo_personagem, &esqueleto);
+
+	while(rodando){	
+	int nova_posicao;
+	
+	
+	char comando;
+	limpar_tela();
+
+	atual(novo_personagem->posicao);
+	imprimir_menu();
+	scanf("%c", &comando);
+	switch (comando){
+		case 'O':
+			olhar(novo_personagem->posicao);
+			pausar();
+			break;
+		case 'S':
+			status_personagem(*novo_personagem);
+			pausar();
+			break;
+		case 'M':
+			printf("digite a nova posicao: ");
+			scanf("%i", &nova_posicao);
+			mover(nova_posicao, novo_personagem);
+			pausar();
+			break;
+		case 'C':
+			combate(novo_personagem, &esqueleto);
+			break;
+		case 'Q':
+			rodando = 0;
+			break;
+		
+		default:
+			printf("comando invalido\n");
+			break;
+		}
+	}
+	
 
 	//GRAVAÇÃO DA POSIÇÃO NA HISTORIA
-	/*FILE *gravar;
+	FILE *gravar;
 	gravar = fopen("historia.dat", "wb");
 	char texto[TAM_TEX_MAXIMO];
 	fgets(texto,TAM_TEX_MAXIMO, stdin);
 	fwrite(texto, sizeof(texto), 1, gravar);
 	fgets(texto, TAM_TEX_MAXIMO, stdin);
 	fwrite(texto, sizeof(texto), 1, gravar);
-	fclose(gravar);*/
+	fclose(gravar);
 	
 	//ANDAMENTO DA HISTÓRIA
 
@@ -51,7 +94,7 @@ int main(){
 		atual();
 	}*/
 
-	//free(novo_personagem); //liberar memória alocada
+	free(novo_personagem); //liberar memória alocada
 	return 0;
 }
 void menu_principal(){
@@ -59,9 +102,15 @@ void menu_principal(){
 
 	while(rodando){
 		logo();
-		printf("\n\n\t\t\t\t\t\t\t\t\t|1-Novo Jogo\n\t\t\t\t\t\t\t\t\t|2-Continuar\n\t\t\t\t\t\t\t\t\t|3-Ranking\n\t\t\t\t\t\t\t\t\t|4-Desenvolvedores\n\t\t\t\t\t\t\t\t\t|0-Fechar Programa\n\n");
-		scanf("%d", &escolha);
+		printf("\n\n\t\t\t\t\t\t\t\t\t|1-Novo Jogo",sleep(1));
+		printf("\n\t\t\t\t\t\t\t\t\t|2-Continuar", sleep(1));
+		printf("\n\t\t\t\t\t\t\t\t\t|3-Ranking", sleep(1));
+		printf("\n\t\t\t\t\t\t\t\t\t|4-Desenvolvedores", sleep(1));
+		printf("\n\t\t\t\t\t\t\t\t\t|0-Fechar Programa\n\t\t\t\t\t\t\t\t\t", sleep(1));
 
+		scanf("%d", &escolha);
+		
+		limpar_tela();
         switch (escolha){
         case 1:
         //chama a criação de personagem
@@ -80,16 +129,13 @@ void menu_principal(){
         
         case 4:
             limpar_tela();
-            printf("Fernando Brawl Stars\nGustavo Brawlhalla\nLohan");
-			
-            printf("clique em qualquer tecla para voltar");
-			system("pause");
+            printf("Fernando Brawl Stars\nGustavo Brawlhalla\nLohan\n");
+            pausar();
             break;
-            
-        case 0:
-        	limpar_tela();
-        	rodando = 0;
-        	break;
+		case 0:
+			rodando = 0;
+			break;
+        
 
         default:
             break;
@@ -102,38 +148,53 @@ PERSONAGEM *criarPersonagem(){
 	PERSONAGEM *novo_personagem = (PERSONAGEM*) malloc(sizeof(PERSONAGEM));
 	int classe=0;
 	
-	printf("Primeiro de tudo, escolha sua classe:\n|1-Guerreiro\n|2-Arqueiro\n|3-Paladino\n");
-	
+	printf("Primeiro de tudo, escolha sua classe:\n");
+	printf("|1-Guerreiro\n", sleep(1));
+	printf("|2-Arqueiro\n",sleep(1));
+	printf("|3-Paladino\n", sleep(1));
+	printf("Sua escolha:", sleep(1));
 	while(classe < 1 || classe > 3){
 		scanf("%d", &classe);
+		limpar_tela();
 		switch (classe){
 		case 1:
 			printf("Voce escolheu o guerreiro");
-			novo_personagem->HP = 120;
-			novo_personagem->ATK = 20;
-			novo_personagem->DEF = 20;
-			novo_personagem->SPD = 10;
+			novo_personagem->HP = 12;
+			novo_personagem->ATK = 2;
+			novo_personagem->DEF = 2;
+			novo_personagem->SPD = 11;
 			break;
 		case 2:
 			printf("Voce escolheu o arqueiro");
-			novo_personagem->HP = 100;
-			novo_personagem->ATK = 40;
-			novo_personagem->DEF = 10;
+			novo_personagem->HP = 10;
+			novo_personagem->ATK = 4;
+			novo_personagem->DEF = 1;
 			novo_personagem->SPD = 30;
 			break;
 		case 3:
 			printf("Voce escolheu o paladino");
-			novo_personagem->HP = 130;
-			novo_personagem->ATK = 10;
-			novo_personagem->DEF = 40;
+			novo_personagem->HP = 14;
+			novo_personagem->ATK = 1;
+			novo_personagem->DEF = 4;
 			novo_personagem->SPD = 9;
 			break;
-		}	
-	}
-	
-	printf("\nAgora, escolha o seu nome: ");
+		
+		default:
+			printf("Classe invalidade. Tente novamente!");
+			continue;
+		}
+
+	}sleep(1);
+	novo_personagem->posicao = 0;
+	limpar_tela();
+	printf("Agora, escolha o seu nome: ");
 	scanf("%s", novo_personagem->nome);
-	printf("Seu personagem foi criado!\nSeus atributos sao:\nHP %i\nATAQUE %i\nDEFESA %i", novo_personagem->HP, novo_personagem->ATK, novo_personagem->DEF);
+	limpar_tela();
+	sleep(1);
+	printf("Seu personagem foi criado!");
+	printf("\nSeus atributos sao:\nHP %i\nATAQUE %i\nDEFESA %i",novo_personagem->HP, novo_personagem->ATK, novo_personagem->DEF,sleep(1));
+	pausar();
+	limpar_tela();
 	return novo_personagem;
 }
 //PRINTA OS ATRIBUTOS E POSIÇÃO DO PERSONAGEM
@@ -143,3 +204,35 @@ void status_personagem(PERSONAGEM personagem){
 void limpar_tela(){
 	system("cls");
 }
+void atual(int pagina){
+	FILE *fp;
+		char texto[TAM_TEX_MAXIMO];
+		fp = fopen("historia.dat", "rb");
+		fseek(fp, pagina * TAM_TEX_MAXIMO * sizeof(char), SEEK_SET);
+		fread(texto, sizeof(texto), 1, fp);
+		printf("%s\n", texto);
+		fclose(fp);
+		}
+void olhar(int posicao){
+	FILE *fp;
+	char texto[TAM_TEX_MAXIMO];
+	fp = fopen("olhar.dat", "rb");
+	fseek(fp, posicao * TAM_TEX_MAXIMO * sizeof(char), SEEK_SET);
+	fread(texto, sizeof(texto), 1, fp);
+	printf("%s", texto);
+	fclose(fp);
+}
+void mover(int posicao, PERSONAGEM *novo_personagem){
+	novo_personagem->posicao = posicao;
+	printf("voce avancou para a pagina %i\n", posicao);
+}
+
+void pausar() {
+    
+    fflush(stdin);
+    getchar();
+}
+void imprimir_menu()	
+		{
+		printf("%10s %10s %10s %10s %10s\n", "[O] Olhar", "[S] Status", "[M] Mover", "[C] combate", "[Q] Sair");
+		}
