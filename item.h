1e70650d0fcb251bf3fe28e069bca8b8f1ab584a
Lohan Vieira <lohan10nascimento@gmail.com>
2024-06-28 14:08:52 -0300

    nada

--- item.h
+++ item.h
@@ -1,53 +1,55 @@
-
-void gerar_item(ITEM *item, char *nome_item, int bonus_ataque, int bonus_vida, PERSONAGEM *personagem){
-
-    //verifica se o item ja existe no inventario
-    for(int i = 0; i < personagem->qnt_itens; i++){
-        if(strcmp(personagem->item[i].nome, nome_item)== 0){
-            printf("Voce ja tem esse item!");
+void gerar_item(ITEM *item, char *nome_item, int bonus_ataque, int bonus_vida, PERSONAGEM *personagem) {
+    // Verifica se o item já existe no inventário
+    for (int i = 0; i < personagem->qnt_itens; i++) {
+        if (strcmp(personagem->item[i].nome, nome_item) == 0) {
+            printf("Você já tem esse item!\n");
             return;
         }
     }
-    strcpy(item->nome, nome_item);
-    item->atk = bonus_ataque;
-    item->hp = bonus_vida;
-    
-    
-    strcpy(personagem->item[personagem->qnt_itens].nome,item->nome );
-    personagem->item[personagem->qnt_itens].atk = item->atk;
-    personagem->item[personagem->qnt_itens].hp = item->hp;
-    personagem->qnt_itens++;
-    
+
+    if (personagem->qnt_itens < 3) {
+        strcpy(item->nome, nome_item);
+        item->atk = bonus_ataque;
+        item->hp = bonus_vida;
+        
+        strcpy(personagem->item[personagem->qnt_itens].nome, item->nome);
+        personagem->item[personagem->qnt_itens].atk = item->atk;
+        personagem->item[personagem->qnt_itens].hp = item->hp;
+        personagem->qnt_itens++;
+    } else {
+        printf("Inventário cheio!\n");
+    }
 }
+void printar_inventario(PERSONAGEM *personagem) {
+    printf("Inventário:\n");
+    for (int i = 0; i < personagem->qnt_itens; i++) {
+        printf("%d - %s (ATK: %d, VIDA: %d)\n", i + 1, personagem->item[i].nome, personagem->item[i].atk, personagem->item[i].hp);
+    }
+
+    int item_usar;
+    printf("Digite o número do item para usar ou 0 para voltar: ");
+    scanf("%d", &item_usar);
+
+    if (item_usar > 0 && item_usar <= personagem->qnt_itens) {
+        usar_item(item_usar - 1, personagem);
+    }
+}
+
 void usar_item(int indice, PERSONAGEM *personagem) {
-    ITEM item = personagem->item[indice];
-    personagem->ATK += item.atk;
-    personagem->HP += item.hp;
-    // Copia o último item do inventário para a posição do item usado
-    personagem->item[indice] = personagem->item[personagem->qnt_itens - 1];
-    // Diminui a quantidade de itens no inventário
-    personagem->qnt_itens--;
-}
+    if (indice >= 0 && indice < personagem->qnt_itens) {
+        ITEM item = personagem->item[indice];
 
+        // Aplica os efeitos do item
+        personagem->ATK += item.atk;
+        personagem->HP += item.hp;
+        // Remove o item do inventário
+        for (int i = indice; i < personagem->qnt_itens - 1; i++) {
+            personagem->item[i] = personagem->item[i + 1];
+        }
+        personagem->qnt_itens--;
 
-void printar_inventario(PERSONAGEM *personagem){
-    int escolha;
-    printf("----------------------------SEU INVENTARIO----------------------------");
-    for(int i = 0; i < personagem->qnt_itens;i++){
-        printf("\n\t\t\t\t%i\n%s \nbonus dmg:%i \nbonus hp:%i\n", i + 1 ,personagem->item[i].nome, personagem->item[i].atk, personagem->item[i].hp);
-        printf("----------------------------------------------------------------------");
-
-    }
-    printf("\nUsar item:");
-    scanf("%i", &escolha);
-    if(escolha > 0 && escolha <= personagem->qnt_itens){
-        usar_item(escolha - 1, personagem);
-        printf("voce usou %s\n.", personagem->item[escolha - 1].nome);
-    }
-    else{
-        printf("escolha invalida\n");
+        printf("Você usou %s!\n", item.nome);
+    } else {
+        printf("Índice de item inválido!\n");
     }
 }
-
-
-
