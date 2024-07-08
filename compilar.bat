0229c568249a858bb91099453f7a5f5423457a4c
Lohan Vieira <lohan10nascimento@gmail.com>
2024-07-08 13:49:10 -0300

    tinha sumido

---
+++ compilar.bat
@@ -0,0 +1,15 @@
+@echo off
+rem Script de compilação e execução do jogo em C
+
+rem Compilação
+gcc -o ./bin/Game.exe src\main.c src\combate.c src\item.c src\andamento.c -Iinclude -lm
+
+rem Verifica se a compilação teve êxito
+if %errorlevel% neq 0 (
+    echo Compilação falhou. Verifique os erros acima.
+    pause
+    exit /b %errorlevel%
+)
+
+
+pause
